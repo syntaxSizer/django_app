4c857bf48a8a06ed32fdf78f8878340d4feb3773
syntaxSizer <kakuma4never@gmail.com>
2016-07-31 17:07:29 +0300

    add djsngo queryset to display post list

--- skyluck/settings.py
+++ skyluck/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'Africa/Nairobi'
+TIME_ZONE ='Africa/Nairobi'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
