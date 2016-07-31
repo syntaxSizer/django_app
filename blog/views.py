804ede95deeb63f3847891742b374df47a5caca4
syntaxSizer <kakuma4never@gmail.com>
2016-07-31 17:19:28 +0300

    change variable naming

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from .models import Post
-from.utils import timezone
+from django.utils import timezone
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.filter(published_date_lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html',{'posts ': posts})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
   
 
