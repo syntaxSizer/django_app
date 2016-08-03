29036fcac1c808b99c03cb358035dac5178ca355
syntaxSizer <kakuma4never@gmail.com>
2016-08-03 17:21:41 +0300

    change project static files settings

--- skyluck/settings.py
+++ skyluck/settings.py
@@ -122,7 +122,7 @@ ALLOWED_HOSTS = ['*']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
